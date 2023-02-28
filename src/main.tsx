d1e1424ba77847c0338aaf45eb585a3611fba961
azamatreactjs <azamatreactjs@gmail.com>
2023-02-28 23:27:04 +0500

    Handling not found errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Root } from './routes/Root'
+import { ErrorPage } from './routes/ErrorPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root />
+    element: <Root />,
+    errorElement: <ErrorPage />
   }
 ])
 
